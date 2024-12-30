894b9910eff9d72b68f00b0be46bad50b77138ed
Aren <aren@bransfer.io>
2024-12-30 22:28:18 +0400

    feat: add watchdog contract integration for FuelWallet withdrawal process

--- components/Swap/Withdraw/Wallet/FuelWalletWithdrawal.tsx
+++ components/Swap/Withdraw/Wallet/FuelWalletWithdrawal.tsx
@@ -9,8 +9,10 @@ import { ButtonWrapper, ConnectWalletButton } from './WalletTransfer/buttons';
 import {
     useWallet as useFuelWallet,
 } from '@fuels/react';
+import { bn, Contract, Provider } from 'fuels';
+import WatchdogAbi from '../../../../lib/abis/WatchdogFuelContract.json';
 
-const FuelWalletWithdrawStep: FC<WithdrawPageProps> = ({ network, callData, swapId }) => {
+const FuelWalletWithdrawStep: FC<WithdrawPageProps> = ({ network, callData, swapId, sequenceNumber, depositAddress, amount, token }) => {
     const [loading, setLoading] = useState(false);
     const { setSwapTransaction } = useSwapTransactionStore()
 
@@ -24,9 +26,22 @@ const FuelWalletWithdrawStep: FC<WithdrawPageProps> = ({ network, callData, swap
 
             if (!fuelWallet) throw Error("Fuel wallet not connected")
             if (!callData) throw Error("Call data not found")
+            if (!network) throw Error("Network not found")
+            if (!depositAddress) throw Error("Deposit address not found")
+            if (!network.metadata.watchdog_contract) throw Error("Watchdog contract not found")
 
-            const tx = JSON.parse(callData)
-            const transactionResponse = await fuelWallet.sendTransaction(tx)
+            const provider = await Provider.create(network?.node_url);
+            const contract = new Contract(network.metadata?.watchdog_contract, WatchdogAbi, provider);
+
+            const data = await contract.functions
+                .watch(sequenceNumber)
+                .addTransfer({
+                    destination: depositAddress as string,
+                    amount: bn(amount),
+                    assetId: token?.contract!,
+                }).getTransactionRequest();
+
+            const transactionResponse = await fuelWallet.sendTransaction(data)
 
             if (swapId && transactionResponse) setSwapTransaction(swapId, BackendTransactionStatus.Completed, transactionResponse.id)
 
